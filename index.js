5f08d5d054d0e3f4676052fba8b6e1f8c033a3ff
Gilbert Torchon <gilbert@noukod.com>
2019-02-08 15:24:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+  drivers.forEach(function(driver){
+    console.log(driver.name);
+  })
+}
+
+function logDriversByHometown(drivers,hometown){
+ const byHomeTown=drivers.filter(function(driver){
+    return driver.hometown===hometown
+  })
+
+  logDriverNames(byHomeTown)
+}
+
+function driversByRevenue(drivers){
+  return drivers.slice().sort(function(a,b){
+    return a.revenue - b.revenue
+  })
+}
+
+function driversByName(drivers){
+  return drivers.slice().sort(function(a,b){
+    return a.name.localeCompare(b.name);
+  })
+}
+
+function totalRevenue(drivers){
+  return drivers.reduce(function(total,currentDriver){
+    return total+currentDriver.revenue
+  },0)
+}
+
+function averageRevenue(drivers){
+  return totalRevenue(drivers)/drivers.length
+}
