7f8ee6e5ed1cab9fbebdfc0f931283ccf37f0eaf
Ricartho Franck <ricartho.franck@esih.edu>
2019-09-11 21:10:24 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function logDriverNames(drivers){
+  drivers.forEach(function(el){
+    console.log(el.name);
+  })
+}
+
+function logDriversByHometown(drivers,city){
+  drivers.forEach(function(el){
+    if(el.hometown === city){console.log(el.name);}
+  })
+}
+
+function driversByRevenue(drivers){
+  const newArray = [];
+  return drivers.slice().sort( function(a, b){
+    return  a.revenue - b.revenue
+  } );
+  return newArray.push(drivers);
+}
+
+function driversByName(drivers){
+  const newArray = [];
+  return drivers.slice().sort( function(a, b){
+    return  a.name.localeCompare(b.name);
+  } );
+  return newArray.push(drivers);
+}
